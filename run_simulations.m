ae2e2b2d11041c1b95a139192e70b335c852086f
Bill Tubbs <bill.tubbs@me.com>
2023-06-26 20:09:17 -0700

    Updated with new directory names

--- run_simulations.m
+++ run_simulations.m
@@ -18,14 +18,14 @@ sims_dir = "simulations";
 % sim_name = "test_sim_gpr4";  % Gaussian process models - unfitted basis func2
 % sim_name = "test_sim_fp1";  % First-principles model (zero order)
 % sim_name = "test_sim_fp2";  % First-principles model (adaptive 0/1st order)
-% sim_name = "test_sim_lin";  % Linear model
+sim_name = "test_sim_lin";  % Linear model
 % TODO: For some reason the linear model simulation is very slow
 % sim_name = "test_sim_ens";  % Ensemble model  NOT YET WORKING, need y_sigma estimate
 % sim_name = "test_sim_true";  % test optimizer with true system model
 % sim_name = "test_sim_multiple";
 % sim_name = "sim_true_popt_w";  % param optimization for w parameter
 % sim_name = "sim_gpr_popt_z";  % param optimization for z parameter
-sim_name = "sim_gpr_popt_z2";  % param opt for z parameter with LoadObjFunc2
+% sim_name = "sim_gpr_popt_z2";  % param opt for z parameter with LoadObjFunc2
 
 % WARNING: The following simulations take a long time! ~5 hrs
 % sim_name = "sim_all_eval";  % run all optimizer evaluation simulations
